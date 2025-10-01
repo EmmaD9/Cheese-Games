c8156cd7b2fd56250a9a0922109614b45b8037ac
Bobbits <bobbitsc@gmail.com>
2025-10-01 17:05:02 -0400

    Added bounds box to milking minigame
    Bounds can be changed in a field

--- Cheese-Games/Assets/Scripts/MilkingManager.cs
+++ Cheese-Games/Assets/Scripts/MilkingManager.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MilkingManager : MonoBehaviour
 {
+    [SerializeField] float winThreshold;
+    
 
     private MouseMovement mouseReference;
 
@@ -16,7 +19,8 @@ public class MilkingManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (MouseMovement.totalDistance / 100 >= 200f)
+
+        if (MouseMovement.totalDistance / 100 >= winThreshold)
         {
             Debug.Log("YOU WIN");
         }
--- Cheese-Games/Assets/Scripts/MouseMovement.cs
+++ Cheese-Games/Assets/Scripts/MouseMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 
 public class MouseMovement : MonoBehaviour
 {
+    [SerializeField] Rect bounds = new Rect(100, 100, 400, 400);
 
     private bool isHolding = false;
     public static float totalDistance = 0f;
@@ -26,7 +27,24 @@ public class MouseMovement : MonoBehaviour
     {
         if (!isHolding) return;
 
-        Vector2 delta = context.ReadValue<Vector2>();
-        totalDistance += Mathf.Abs(delta.y);
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+        Vector3 mousePosWorld = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        if (bounds.Contains(mousePosWorld))
+        {
+            Vector2 delta = context.ReadValue<Vector2>();
+            totalDistance += Mathf.Abs(delta.y);
+        }
+        
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Vector3 bottomLeftCor = new Vector3(bounds.xMin, bounds.yMin, 0);
+        Vector3 topRightCor = new Vector3(bounds.xMax, bounds.yMax, 0);
+        Vector3 center = (bottomLeftCor + topRightCor) * 0.5f;
+        Vector3 size = new Vector3(bounds.width, bounds.height, 0);
+        Gizmos.DrawWireCube(center, size);
     }
 }
