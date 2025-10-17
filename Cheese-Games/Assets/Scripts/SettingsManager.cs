787f88b9a24136f8c513df4b5e207945096698a5
EmmaD9 <99094133+EmmaD9@users.noreply.github.com>
2025-10-16 21:38:38 -0400

    master volume works!!

--- Cheese-Games/Assets/Scripts/SettingsManager.cs
+++ Cheese-Games/Assets/Scripts/SettingsManager.cs
@@ -9,13 +9,11 @@ public class SettingsMenu : MonoBehaviour
     public Slider volumeSlider;
     //public Toggle fullscreenToggle;
 
-    private int currentResolutionIndex = 0;
-    private Resolution[] resolutions;
 
     void Start()
     {
         // Load saved settings or use default values
-        LoadSettings();
+        //LoadSettings();
         
         // Add listeners to UI elements
         volumeSlider.onValueChanged.AddListener(delegate { ChangeVolume(); });
@@ -28,26 +26,4 @@ public class SettingsMenu : MonoBehaviour
         PlayerPrefs.Save();
     }
 
-    public void ChangeFullscreen(bool isFullscreen)
-    {
-        Screen.fullScreen = isFullscreen;
-        PlayerPrefs.SetInt("Fullscreen", isFullscreen ? 1 : 0);
-        PlayerPrefs.Save();
-    }
-
-    public void LoadSettings()
-    {
-        // Load volume
-        if (PlayerPrefs.HasKey("Volume"))
-        {
-            volumeSlider.value = PlayerPrefs.GetFloat("Volume");
-            AudioListener.volume = volumeSlider.value;
-        }
-        else
-        {
-            volumeSlider.value = 1f; // Default volume
-            AudioListener.volume = 1f;
-        }
-
-    }
 }
