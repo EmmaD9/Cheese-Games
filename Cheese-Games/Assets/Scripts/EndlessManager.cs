b2a2700c29c94c2a145a4cf8f71db19a26986a4f
EmmaD9 <99094133+EmmaD9@users.noreply.github.com>
2025-10-16 22:03:56 -0400

    updating the endless manager

--- Cheese-Games/Assets/Scripts/EndlessManager.cs
+++ Cheese-Games/Assets/Scripts/EndlessManager.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndlessManager : MonoBehaviour
 {
+    [SerializeField] private string sceneA;
+    [SerializeField] private string sceneB;
+    [SerializeField] private string sceneC;
+
+    //Add in later:
+    //[SerializeField] private string sceneD;
+    //[SerializeField] private string sceneE;
+    //[SerializeField] private string sceneF;
+
+    private List<string> possibleScenes;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        possibleScenes = new List<string>() {sceneA, sceneB, sceneC };
     }
 
-    // Update is called once per frame
-    void Update()
+    public void NextScene()
     {
+        int randomNumberInt = Random.Range(0, possibleScenes.Count);
+
+        string randomScene = possibleScenes[randomNumberInt];
+
+        if (randomScene != null && randomScene != SceneManager.GetActiveScene().name)
+        {
+            Debug.Log("we should go to the next scene now!!!");
+            SceneManager.LoadScene(randomScene);
+        } else
+        {
+            Debug.Log("the scene is either null or it is the current scene");
+            NextScene();
+        }
         
+
     }
 }
