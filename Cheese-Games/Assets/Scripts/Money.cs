d4e689017b8c30b02bab87098ccd36dd94e3e768
EmmaD9 <99094133+EmmaD9@users.noreply.github.com>
2025-10-30 22:27:48 -0400

    money appears on farm scene!!

--- Cheese-Games/Assets/Scripts/Money.cs
+++ Cheese-Games/Assets/Scripts/Money.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Money : MonoBehaviour
 {
     [SerializeField] private int money = 0;
     [SerializeField] private int minMoney; // max money you can earn
     [SerializeField] private int maxMoney; // min money you can earn
+
+    private TextMeshProUGUI moneyText;
 
     private System.Random rand = new System.Random();
 
@@ -20,7 +23,7 @@ public class Money : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckSceneAndFindObject();
     }
 
     public void EarnMoney(int amount)
@@ -46,4 +49,39 @@ public class Money : MonoBehaviour
             return false;
         }
     }
+
+
+    void CheckSceneAndFindObject()
+    {
+        string targetSceneName = "Farm Scene";
+        string targetObjectName = "Money Text";
+        string currentScene = SceneManager.GetActiveScene().name;
+
+        if (currentScene == targetSceneName)
+        {
+            GameObject obj = GameObject.Find(targetObjectName);
+            if (obj != null)
+            {
+                TextMeshProUGUI foundText = obj.GetComponent<TextMeshProUGUI>();
+                if (foundText != null)
+                {
+                    Debug.Log($"Found object '{targetObjectName}' in scene '{targetSceneName}'.");
+                    foundText.text = money.ToString();
+                }
+                else
+                {
+                    Debug.LogWarning($"'{targetObjectName}' exists but does not have a TextMeshProUGUI component.");
+                }
+            }
+            else
+            {
+                Debug.LogWarning($"Object '{targetObjectName}' not found in scene '{targetSceneName}'.");
+            }
+        }
+        else
+        {
+            Debug.Log($"Current scene is '{currentScene}', not '{targetSceneName}'.");
+        }
+    }
+
 }
