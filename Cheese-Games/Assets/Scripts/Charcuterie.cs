b3a6c77ad211ef065dba56fc6f34e1bf147d527f
Bobbits <bobbitsc@gmail.com>
2025-11-13 16:44:54 -0500

    Charcuterie minigame working
    Minigame seems to work correctly

---
+++ Cheese-Games/Assets/Scripts/Charcuterie.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charcuterie : MonoBehaviour
+{
+    [SerializeField] WinScript win;
+    [SerializeField] LoseScript lose;
+    [SerializeField] Timer time;
+
+    [SerializeField] private GameObject board;
+    private Bounds bounds;
+    private List<GameObject> boardObjects = new List<GameObject>();
+    [SerializeField] private int winCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer spriteRenderer = board.GetComponent<SpriteRenderer>();
+        bounds = spriteRenderer.bounds;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int objectsOnBoard = 0;
+
+        foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Draggable"))
+        {
+            SpriteRenderer spriteRenderer = obj.GetComponent<SpriteRenderer>();
+            if (spriteRenderer == null) continue;
+
+            Vector3 center = spriteRenderer.bounds.center;
+
+            if (bounds.Contains(center))
+            {
+                objectsOnBoard++;
+            }
+        }
+
+        if (objectsOnBoard >= winCount && !lose.gameLost)
+        {
+            win.Win();
+        }
+
+        // LOSE CONDITION
+        if (time.outOfTime() && !win.gameWon)
+        {
+            lose.Lose();
+        }
+    }
+}
