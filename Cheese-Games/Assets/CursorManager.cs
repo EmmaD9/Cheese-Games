e8d5a2452d4a4b610dddce6ffb4503771df74521
EmmaD9 <99094133+EmmaD9@users.noreply.github.com>
2025-11-13 19:58:45 -0500

    adding a custom cursor?

---
+++ Cheese-Games/Assets/CursorManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCursor : MonoBehaviour
+{
+    [SerializeField] private Texture2D cursorTexture;
+    public Vector2 hotSpot = Vector2.zero; // Offset from top-left corner
+
+   
+    // Example: Change cursor on mouse enter/exit of an object with a collider
+    void OnSceneStart()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, CursorMode.Auto);
+    }
+
+    void OnSceneExit()
+    {
+        // Revert to the default system cursor
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+}
